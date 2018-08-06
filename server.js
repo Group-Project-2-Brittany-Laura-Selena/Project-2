69637dd14462d95caa905fa485477c62a97a434a
bwtollestr <bwtollestrup@gmail.com>
2018-08-06 17:35:37 -0600

    updates

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
 require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "root",
-  database: "project2_db"
-});
-
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
