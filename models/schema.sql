cbee3c23caf9fd0244d1420ad2b2cc251e1b1a84
bwtollestr <bwtollestrup@gmail.com>
2018-08-09 13:35:43 -0600

    updates to db/table

--- models/schema.sql
+++ models/schema.sql
@@ -2,3 +2,18 @@ DROP DATABASE IF EXISTS talent_db;
 CREATE DATABASE talent_db;
 USE talent_db;
 
+CREATE TABLE `talent` (
+	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR( 255) NOT NULL,
+	`email` VARCHAR( 255 ) NOT NULL,
+	`location` VARCHAR( 255 ) NOT NULL,
+	`website` VARCHAR( 255 ) NOT NULL,
+	`social` VARCHAR( 255 ) NOT NULL,
+    `travel` BOOLEAN NOT NULL,
+	PRIMARY KEY ( `id` )
+);
+
+INSERT INTO talent (name, email, location, website, social, travel)
+VALUES ("DJ S", "djs@test.com", "SLC, UT", "djs.com", "instagram, facebook, youtube", TRUE);
+
+SELECT * FROM talent;
