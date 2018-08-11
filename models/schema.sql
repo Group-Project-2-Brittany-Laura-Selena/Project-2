fa663daf4355649ff5c9e21e6203ff04a97c5b48
bwtollestr <bwtollestrup@gmail.com>
2018-08-11 12:31:01 -0600

    updates

--- models/schema.sql
+++ models/schema.sql
@@ -4,18 +4,22 @@ USE talent_db;
 
 CREATE TABLE `talent` (
 	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-	`name` VARCHAR( 150 ) NOT NULL,
-	`bio` VARCHAR( 500 ) NOT NULL,
+	`artist_name` VARCHAR( 150 ) NOT NULL,
+	`specialty` VARCHAR( 150 ) NOT NULL,
+	`bio` VARCHAR( 750 ) NOT NULL,
 	`email` VARCHAR( 150 ) NOT NULL,
-	`location` VARCHAR( 300 ) NOT NULL,
+	`city` VARCHAR( 250 ) NOT NULL,
+	`state` VARCHAR( 250 ) NOT NULL,
 	`website` VARCHAR( 225 ) NOT NULL,
-	`social` VARCHAR( 500 ) NOT NULL,
+	`instagram` VARCHAR( 500 ),
+	`facebook` VARCHAR( 500 ),
+	`youtube` VARCHAR( 500 ),
 	`specialty` VARCHAR( 150 ) NOT NULL,
     `travel` BOOLEAN NOT NULL,
 	PRIMARY KEY ( `id` )
 );
 
-INSERT INTO talent (name, bio, email, location, website, social, specialty, travel)
+INSERT INTO talent (artist_name, specialty, bio, email, city, state, website, instagram, facebook, youtube, specality, travel)
 VALUES ("DJ S", "BIO", "live@test.com", "SLC, UT", "djs.com", "instagram, facebook, youtube", "DJ", TRUE);
 
 SELECT * FROM talent;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,29 +3,29 @@ var db = require("../models");
 
 module.exports = function (app) {
     // Get all examples
-    app.get("/api/examples", function (req, res) {
-        db.Example.findAll({}).then(function (dbExamples) {
-            res.json(dbExamples);
+    app.get("/api/home", function (req, res) {
+        db.Example.findAll({}).then(function (talent_db) {
+            res.json(talent_db);
         });
     });
 
     // Create a new example
     app.post("/api/examples", function (req, res) {
-        db.Example.create(req.body).then(function (dbExample) {
-            res.json(dbExample);
+        db.Example.create(req.body).then(function (talent_db) {
+            res.json(talent_db);
         });
     });
 
     // Delete an example by id
-    app.delete("/api/examples/:id", function (req, res) {
-        db.Example.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbExample) {
-            res.json(dbExample);
-        });
-    });
+    // app.delete("/api/examples/:id", function (req, res) {
+    //     db.talent.destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     }).then(function (talent_db) {
+    //         res.json(talent_db);
+    //     });
+    // });
 };
 //original master
 // Requiring our models and passport as we've configured it
