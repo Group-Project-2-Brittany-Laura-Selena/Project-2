1cb4d07a39090054f131c7d8a43efa0be77e9413
Maniacal Laura <maniacallaura@gmail.com>
2018-08-11 23:26:09 -0600

    results and modal updated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,14 +2,23 @@
 var db = require("../models");
 
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
+//these get requests are for the user, not the artist. Here they can search for local talent they'd like to hire
+  app.get("/", function (req, res) {
+    res.render("home", {
+    });
+  });
 
-//new master
+  app.get("/results", function (req, res){
+    res.render("results", {
+    })
+  })
+//these get requests are for the artist to make and login to their profiles. 
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -21,23 +30,9 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the talent page
     if (req.user) {
-      res.redirect("/");
+      res.redirect("/talent");
     } else {
       res.render("login");
-    }
-  });
-
-  app.get("/", function (req, res) {
-    if (req.user) {
-      db.User.findAll({}).then(function (dbUsers) 
-      {
-        res.render("home", {
-          msg: "Welcome!",
-          users: dbUsers
-        });
-      });
-    } else {
-      console.log("home not found");
     }
   });
 
@@ -57,14 +52,14 @@ module.exports = function(app) {
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
-//original master
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+// //original master
+//   app.get("/", function(req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/signup.html"));
+//   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
@@ -74,12 +69,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+//   // Here we've add our isAuthenticated middleware to this route.
+//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+//   app.get("/members", isAuthenticated, function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/members.html"));
 
-  });
+//   });
 
 });
 }
